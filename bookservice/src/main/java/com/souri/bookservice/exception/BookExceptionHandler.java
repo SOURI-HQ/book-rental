c850ff9f51a9fa4205a1c7a6d51a0ef7a746011e
SOURI-HQ <seconddan@wp.pl>
2023-07-26 22:31:11 +0200

    feat: add custom 405 exception handler

--- bookservice/src/main/java/com/souri/bookservice/exception/BookExceptionHandler.java
+++ bookservice/src/main/java/com/souri/bookservice/exception/BookExceptionHandler.java
@@ -1,10 +1,23 @@
 package com.souri.bookservice.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.lang.Nullable;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
+
+import java.io.IOException;
 
 @RestControllerAdvice
 public class BookExceptionHandler {
@@ -25,7 +38,11 @@ public class BookExceptionHandler {
         return buildErrorResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, errorMessage));
     }
 
-    // TODO: add method not allowed exception handler
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<Object> handleMethodNotSupportedException(HttpServletRequest request, HttpRequestMethodNotSupportedException e) {
+        String errorMessage = " for " + request.getRequestURI();
+        return buildErrorResponseEntity(new ApiError(HttpStatus.METHOD_NOT_ALLOWED, e.getMessage() + errorMessage));
+    }
 
     private ResponseEntity<Object> buildErrorResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, apiError.getStatus());
